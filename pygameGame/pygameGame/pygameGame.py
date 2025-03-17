99b1c821fd211b81b3dc6805957c88ae6d6fb7c3
VoloshchukMark <voloshchuk.mark@chnu.edu.ua>
2025-03-17 23:40:21 +0200

    Optimize code

--- pygameGame/pygameGame/pygameGame.py
+++ pygameGame/pygameGame/pygameGame.py
@@ -5,50 +5,52 @@ import pygame.draw
 import pygame.key
 pygame.init()
 
+win = pygame.display.set_mode((700, 480))
+pygame.display.set_caption("Mah first game")
+clock = pygame.time.Clock()
+
 playerWalkRight = [pygame.image.load('sprites\player\R1.png'), pygame.image.load('sprites\player\R2.png'), pygame.image.load('sprites\player\R3.png'), pygame.image.load('sprites\player\R4.png'), pygame.image.load('sprites\player\R5.png'), pygame.image.load('sprites\player\R6.png'), pygame.image.load('sprites\player\R7.png'), pygame.image.load('sprites\player\R8.png'), pygame.image.load('sprites\player\R9.png')]
 playerWalkLeft = [pygame.image.load('sprites\player\L1.png'), pygame.image.load('sprites\player\L2.png'), pygame.image.load('sprites\player\L3.png'), pygame.image.load('sprites\player\L4.png'), pygame.image.load('sprites\player\L5.png'), pygame.image.load('sprites\player\L6.png'), pygame.image.load('sprites\player\L7.png'), pygame.image.load('sprites\player\L8.png'), pygame.image.load('sprites\player\L9.png')]
 bg = pygame.image.load('sprites/bg/bg.jpg')
-player = pygame.image.load('sprites\player\standing.png')
+playerIdle = pygame.image.load('sprites\player\standing.png')
 
+class player(object):
+    def __init__(self, x, y, height, width):
+        self.x = x
+        self.y = y
+        self.height = height
+        self.width = width
+        self.velocity = 5
+        self.jumpState = False
+        self.jumpCount = 8
+        self.side = 2           #0 - left, 1 - right, 2 - idle
+        self.walkCount = 0
 
-win = pygame.display.set_mode((700, 500))
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
 
-pygame.display.set_caption("Mah first game")
+        if self.side == 0:
+            win.blit(playerWalkLeft[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        elif player1.side == 1:
+            win.blit(playerWalkRight[self.walkCount//3], (self.x, self.y))
+            self.walkCount += 1
+        else:
+            win.blit(playerIdle, (self.x, self.y))
 
-clock = pygame.time.Clock()
-
-x = 100
-y = 430
-heightR = 64
-widthR = 64
-velocity = 5
-
-jumpState = False
-jumpCount = 8
-
-side = 0    #0 - left, 1 - right, 2 - idle
-walkCount = 0
 
 def redrawGameWindow():
-    global walkCount
     win.blit(bg, (0,0))
 
-    if walkCount + 1 >= 27:
-        walkCount = 0
-
-    if side == 0:
-        win.blit(playerWalkLeft[walkCount//3], (x, y))
-        walkCount += 1
-    elif side == 1:
-        win.blit(playerWalkRight[walkCount//3], (x, y))
-        walkCount += 1
-    else:
-        win.blit(player, (x, y))
+    player1.draw(win)
+    
 
     pygame.display.update()
 
 
 run = True
+player1 = player(100, 400, 64, 64)
 while(run):
     clock.tick(27)
     
@@ -60,39 +62,39 @@ while(run):
     sprint = pygame.key.get_repeat()
 
 
-    if keys[pygame.K_a] and x >= 0:
-        x -= velocity
-        side = 0
-    elif keys[pygame.K_d] and x <= 700:
-        x += velocity
-        side = 1
+    if keys[pygame.K_a] and player1.x >= 0:
+        player1.x -= player1.velocity
+        player1.side = 0
+    elif keys[pygame.K_d] and player1.x <= 700:
+        player1.x += player1.velocity
+        player1.side = 1
     else:
-        side = 2
-        walkCount = 0
+        player1.side = 2
+        player1.walkCount = 0
     if keys[pygame.K_RETURN]:
-        velocity = 10
+        player1.velocity = 10
     else:
-        velocity = 5
+        player1.velocity = 5
 
-    if not jumpState:
+    if not player1.jumpState:
         #if keys[pygame.K_w] and y >= 0:
         #    y -= velocity
         #if keys[pygame.K_s] and y <= 500:
         #    y += velocity
         if keys[pygame.K_SPACE]:
-            jumpState = True
-            side = 2
-            walkCount = 0
+            player1.jumpState = True
+            player1.side = 2
+            player1.walkCount = 0
     else:
-        if jumpCount >= -8:
+        if player1.jumpCount >= -8:
             neg = 1
-            if jumpCount < 0:
+            if player1.jumpCount < 0:
                 neg = -1
-            y -= (jumpCount ** 2) / 2 * neg
-            jumpCount -= 1
+            player1.y -= (player1.jumpCount ** 2) / 2 * neg
+            player1.jumpCount -= 1
         else:
-            jumpState = False
-            jumpCount = 8
+            player1.jumpState = False
+            player1.jumpCount = 8
     
     redrawGameWindow()
         
